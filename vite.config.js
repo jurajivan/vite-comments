75ea22e6fc44e0a6a8954f48aa310a49f7449b50
Juraj Ivan <jurajivan@gmail.com>
2021-02-10 21:52:54 +0100

    Init

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import vue from '@vitejs/plugin-vue'
+
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        comments: true,
+      }
+    }
+  })]
+}
